77c8cc9ecfa964b48864ca77822fb43b9287a26a
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-12-14 16:57:42 -0500

    logout & privateRoute added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { BrowserRouter as Router, Route, Redirect, Link} from 'react-router-dom'
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
+import Logout from './components/Logout';
 
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -13,11 +15,12 @@ function App() {
     <div className="App">
       <header>
         <h1>Friends Database</h1>
-        <Link className="link" to="login">Login</Link>
-        <Link className="link" to="friends">Friends List</Link>
-        <Link className="link" to="friends/add">Add Friends</Link>
-        <Link className ="link" to="friends">Logout</Link>
+        <Link className="link" to="/login">Login</Link>
+        <Link className="link" to="/friends">Friends List</Link>
+        <Link className="link" to="/friends/add">Add Friends</Link>
+        <Link className ="link" to="/logout">Logout</Link>
       </header>
+
       <Route exact path="/">
         <Login/>
       </Route>
@@ -26,13 +29,11 @@ function App() {
         <Redirect to="/"/>
       </Route>
 
-      <Route exact path="/friends">
-        <FriendsList/>
-      </Route>
+      <PrivateRoute exact path="/friends" component={FriendsList}/>
+       
+      <PrivateRoute exact path="/friends/add" component={AddFriend}/>
 
-      <Route exact path="/friends/add">
-        <AddFriend/>
-      </Route>
+      <PrivateRoute exact path="/logout" component={Logout}/>
     </div>
     </Router>
   );
